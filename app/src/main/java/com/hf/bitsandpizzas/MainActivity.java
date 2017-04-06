7da5d80650fc46cfe98d733cdce26ad89129e80d
Derek Poon <thebevil@gmail.com>
2017-04-06 14:05:07 +0100

    First commit

---
+++ app/src/main/java/com/hf/bitsandpizzas/MainActivity.java
@@ -0,0 +1,40 @@
+package com.hf.bitsandpizzas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_create_order:
+                // code to run when the create order item is clicked
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                // code to run when the settings icon is clicked
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
