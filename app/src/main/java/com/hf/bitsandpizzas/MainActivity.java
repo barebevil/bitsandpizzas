3b4f80fb9f31de5a0f23c6bd161975c80ec5d190
Derek Poon <thebevil@gmail.com>
2017-04-07 20:15:49 +0100

    Updated MainActivity.java to populate the navigation drawer

--- app/src/main/java/com/hf/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hf/bitsandpizzas/MainActivity.java
@@ -1,18 +1,36 @@
 package com.hf.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[]titles;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, titles));
     }
 
     @Override
